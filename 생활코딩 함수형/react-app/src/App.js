517302cf5c2df00876edaf8a00336c78998b46dc
chk7082 <whgusrl7082@gmail.com>
2023-07-10 23:58:29 +0900

    0710 until create

--- 생활코딩 함수형/react-app/src/App.js
+++ 생활코딩 함수형/react-app/src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 
 
@@ -34,7 +35,7 @@ function Nav(props) {
       <li key={t.id}>
         <a id={t.id} href={'/read/' + t.id} onClick={(event) => {
           event.preventDefault();
-          props.onChangeMode(event.target.id);
+          props.onChangeMode(Number(event.target.id));
         }}>{t.title}</a>
       </li>)
   }
@@ -48,23 +49,80 @@ function Nav(props) {
   )
 }
 
+function Create(props) {
+  return (<article>
+    <h2>Create</h2>
+    <form onSubmit={(event) => {
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title, body);
+    }}>
+      <p><input type="text" name="title" placeholder="title"/></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create" /></p>
+    </form>
+  </article>)
+}
+
 function App() {
-  const topics = [
+  // let _mode = useState('WELCOME');
+  // let mode = _mode[0];
+  // let setMode = _mode[1];
+  let [mode, setMode] = useState('WELCOME');
+  let [id, setId] = useState(null);
+  
+  let [nextId, setNextId] = useState(4);
+  let [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ..'},
     {id:2, title:'css', body:'css is ..'},
     {id:3, title:'javascript', body:'javascript is ..'},
-  ]
+  ])
+
+  let content = null;
+  if (mode == "WELCOME") {
+    content = <Article title="Welcome" body="Hello, WEB" />
+  } else if (mode == "READ") {
+    let title, body = null;
+    for (let i=0; i<topics.length; i++){
+      if(topics[i].id === id){
+        title = topics[i].title
+        body = topics[i].body
+      }
+    }
+    content = <Article title={title} body={body} />
+  } else if (mode == "CREATE") {
+    content = <Create onCreate={(_title, _body) => {
+      const newTopic = {id:nextId, title: _title, body: _body}
+      const newTopics = [...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId + 1);
+
+    }}/>
+  }
+
   return (
     <div className="App">
       <Header title="WEB" onChangeMode={()=>{
-        alert('Header');
+        setMode('WELCOME');
       }}/>
 
-      <Nav topics={topics} onChangeMode={(id) => {
-        alert(id);
+      <Nav topics={topics} onChangeMode={(_id) => {
+        setMode('READ');
+        setId(_id)
       }}/>
 
-      <Article title="Welcome" body="Hello, WEB" />
+      {/* <Article title="Welcome" body="Hello, WEB" /> */}
+      {content}
+
+      <a href="/create" onClick={(event) => {
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
     </div>
   );
 }
